29ee64d372cce5796242dd2cf66104b30ca3585b
Jason Nathanson <jlnathan@ucsd.edu>
2014-10-21 13:48:36 -0700

    add analysis features

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -1,12 +1,20 @@
 import pandas as pd
-import analyze_mosquito_data_lib as mosquito
+import analyze_mosquito_data_lib as mosquito_lib
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 filename = "A1_mosquito_data.csv"
 
-data = pd.csv_read(filename)
+# read the data
+data = pd.read_csv(filename)
 print data.head()
+
+# celsius to fahrenheit
+data["temperature"] = mosquito_lib.fahr_to_celsius(data["temperature"])
+parameters = mosquito_lib.analyze(data)
+
+# save parameters to file
+parameters.to_csv("parameters.csv")
 
 # c:/anaconda/scripts/ipython.exe analyze_mosquito_data_script.py
 
